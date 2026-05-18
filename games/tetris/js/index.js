68988c7e1423c09ec265efe4964cd37f5311e65e
moyuan10086 <2572045628@qq.com>
2026-05-18 18:04:54 +0800

    Site updated: 2026-05-18 18:04:46

--- games/tetris/js/index.js
+++ games/tetris/js/index.js
@@ -2,7 +2,7 @@ var frame;
 
 function initGame()
 {
-	frame = new GameFrame(38,14,16);
+	frame = new GameFrame(38,10,20);
 	frame.init();
 
    document.body.addEventListener("keydown",MoveOrChange)
